d3de40d5011551b9bd0a81406acfd8112acf2033
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2020-03-05 20:19:58 -0300

    added the connection to the database

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const logger = require('morgan');
 
+require('./config/database')
+
 const indexRouter = require('./app/routes/index');
 
 const app = express();
